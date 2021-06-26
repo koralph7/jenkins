2a21b4c1e6e77513a3cce127e76a16df2dc0e2f7
koralph <koralph@tlen.pl>
2021-06-26 19:09:53 +0200

    first

---
+++ src/main/java/com/example/Jenkins/Main.java
@@ -0,0 +1,20 @@
+package com.example.Jenkins;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Main {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World !";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
